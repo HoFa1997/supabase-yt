d68cd3f9514806492537ff934c93b16be532b208
Hossein Fatemi <71207460+HoFa1997@users.noreply.github.com>
2023-11-26 22:44:57 +0330

    Update create-fun-add-new-user.sql

--- script/create-fun-add-new-user.sql
+++ script/create-fun-add-new-user.sql
@@ -13,3 +13,17 @@ $$ LANGUAGE plpgsql SECURITY DEFINER;
 CREATE TRIGGER on_auth_user_created
 AFTER INSERT ON auth.users
 FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
+
+-- Command to Drop a Trigger
+-- Syntax: DROP TRIGGER trigger_name ON table_name;
+-- Usage: Removes a specified trigger from a table.
+-- Example: DROP TRIGGER on_auth_user_created ON auth.users;
+-- Removes the trigger named on_auth_user_created from the auth.users table.
+DROP TRIGGER on_auth_user_created ON auth.users;
+
+-- Command to Drop a Function
+-- Syntax: DROP FUNCTION function_name([parameter_types]);
+-- Usage: Deletes a specified function from the database.
+-- Example: DROP FUNCTION public.handle_new_user();
+-- Deletes the function public.handle_new_user().
+DROP FUNCTION public.handle_new_user();
