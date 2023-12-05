f5628b0992b3e8365471b85bcdb47c73c7da5fa2
Hossein Fatemi <hfatemi1997@gmail.com>
2023-12-05 21:42:46 +0330

    Fix map function in posts page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
       <h1 className="text-5xl mb-4">Supabase Project</h1>
       <h2 className="text-4xl mb-4">Posts Page</h2>
       {posts &&
-        posts.map((post, index) => (
+        posts.map((post) => (
           <div key={post.post_id} className="border-2 p-4 mb-4 rounded-md">
             <h3 className="text-xl font-bold mb-2">{post.title}</h3>
             <p className="text-gray-700">{post.content}</p>
--- frontend/src/components/provider.tsx
+++ frontend/src/components/provider.tsx
@@ -7,19 +7,6 @@ import { ReactNode, useEffect } from "react";
 export const Provider = ({ children }: { children: ReactNode }) => {
   const { replace } = useRouter();
 
-  useEffect(() => {
-    const fetchSessionData = async () => {
-      const {
-        data: { session },
-      } = await supabaseClient.auth.getSession();
-      if (!session) {
-        replace("/login");
-      }
-    };
-    fetchSessionData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <>
       <SnackbarProvider
